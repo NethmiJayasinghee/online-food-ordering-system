2d86e1b65d9c8fcf58b956ca15312f8eec7ded2e
NethmiJayasinghee <ndhananjana98@gmail.com>
2025-12-27 14:30:29 -0800

    setup 3 files

--- src/main/java/com/icet/online_Food/model/Food.java
+++ src/main/java/com/icet/online_Food/model/Food.java
@@ -1,12 +1,14 @@
 package com.icet.online_Food.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jdk.jfr.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -16,4 +18,23 @@ public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+    private  String description;
+    private Long price;
+
+    @ManyToOne
+    private Category foodCategory;
+
+    @Column(length = 1000)
+    @ElementCollection
+    private List<String> images;
+    private boolean available;
+    @ManyToOne
+    private Restaurant restaurant;
+    private  boolean isVegetarian;
+    private  boolean isSeasonal;
+    @ManyToMany
+    private  List<Ingredientsitem> ingredientsItems=new ArrayList<>();
+    private Date creationDate;
+
 }
